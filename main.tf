5c8f5b58a998ddf1255bbca0208d77bcdfa89d9b
NyulaYorda <nabilaalhassan70@gmail.com>
2023-11-11 13:10:58 +0000

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
-# Configuring the AWS provider
+# Configure the AWS provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Creating a VPC
+# Create a VPC
 resource "aws_vpc" "vcs_vpc" {
   cidr_block = "10.0.0.0/18" 
 
@@ -13,7 +13,7 @@ resource "aws_vpc" "vcs_vpc" {
   }
 }
 
-# Creating a network ACL
+# Create a network ACL
 
 resource "aws_network_acl" "vcs_acl" {
   vpc_id = aws_vpc.vcs_vpc.id 
@@ -21,7 +21,7 @@ resource "aws_network_acl" "vcs_acl" {
 }
 
 
-# Creating two public and two private subnets within the VPC
+# Create two public and two private subnets within the VPC
 resource "aws_subnet" "public_vcs1" {
   vpc_id                  = aws_vpc.vcs_vpc.id
   cidr_block              = "10.0.1.0/24" 
@@ -63,16 +63,17 @@ resource "aws_subnet" "private_vcs2" {
 }
 
 
-# Creating a security group
+# Create a security group
 resource "aws_security_group" "vcs_sg" {
   vpc_id = aws_vpc.vcs_vpc.id
+
   tags = {
     Name = "vcs_sg"
   }
 
 }
 
-#Creating a key pair 
+#Create a key pair 
 resource "aws_key_pair" "vcs_key" {
 key_name = "vcs_key"
 public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfKL8oiMrAvijAqOMa9ajGOb7k7z9a691Gmf8xtaPKFOg0Wqcu35ascM6vjRlaPtpkBA/MDy0bjfPzbZV19LOE92LbUQfwW9aRMBpYACMSPLyR0e2plT4o81QUIOpUIebgjxvpq+DsDOQdCb+MmGz9L0RN8e8vnUV4Pr2jJzOabdQ2aVtIkks2iDJ4Pexq73zpU13Xx8YUWPQmPPU/NhYezSmvZZuL90kSUvsHj1ivu5pwqbYDZqSNAagfIwD9XSOLxhgUUGtQPTcIcXiOdun0O5+WQ+uFw13zU8kBOGU04qliaFu+UZ/IHwNf+qy7L3E+529FsGvBi2kCY+aaP9nT Nabila@PSL-NABILA"
@@ -80,14 +81,15 @@ public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfKL8oiMrAvijAqOMa9ajGOb7k7z
 }
 
 
-# Launching four (4) EC2 instance
+# Launch four (4) EC2 instance
 
 resource "aws_instance" "public_1" {
   ami               = "ami-0e8a34246278c21e4" 
   instance_type     = "t2.micro"              
   key_name          = "vcs_key"    
   subnet_id         = aws_subnet.public_vcs1.id
-  #security_group_id = aws_security_group.vcs_sg.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.vcs_sg.id]
 
   tags = {
     Name = "public 1"
@@ -99,8 +101,8 @@ resource "aws_instance" "private_1" {
   instance_type     = "t2.micro"              
   key_name          = "vcs_key"    
   subnet_id         = aws_subnet.private_vcs1.id
-  #security_group_id = aws_security_group.vcs_sg.id
-
+  vpc_security_group_ids = [aws_security_group.vcs_sg.id]
+ 
   tags = {
     Name = "private 1"
   }
@@ -111,7 +113,8 @@ resource "aws_instance" "public_2" {
   instance_type      = "t2.micro"              
   key_name           = "vcs_key"    
   subnet_id          = aws_subnet.public_vcs2.id
-  #security_group_id  = aws_security_group.vcs_sg.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.vcs_sg.id]
 
   tags = {
     Name = "public 2"
@@ -123,32 +126,37 @@ resource "aws_instance" "private_2" {
   instance_type      = "t2.micro"              
   key_name           = "vcs_key"    
   subnet_id          = aws_subnet.private_vcs2.id
-  #security_group_id  = aws_security_group.vcs_sg.id
+  vpc_security_group_ids = [aws_security_group.vcs_sg.id]
+  
 
   tags = {
     Name = "private 2"
   }
 }
 
-# Creating an Application Load Balancer
+#Create an Internet Gateway
+
+resource "aws_internet_gateway" "vcs_igw" {
+  vpc_id = aws_vpc.vcs_vpc.id 
+
+  tags = {
+    Name = "vcs_igw"
+  }
+}
+
+# Create an Application Load Balancer
 
 
-resource "aws_lb" "vcs_lb"{
-  internal           = false
-  load_balancer_type = "application"
-  #security_groups_id = aws_security_group.vcs_sg.id 
-  subnets            = ["public_vcs1", "public_vcs2"]
-
-  enable_deletion_protection = false 
-
-  enable_http2       = true
+resource "aws_lb" "vcs_alb" {
+  internal                         = false
+  load_balancer_type               = "application"
+  subnets                          = [aws_subnet.public_vcs1.id, aws_subnet.public_vcs2.id]
+  enable_deletion_protection       = false
+  enable_http2                     = true
   enable_cross_zone_load_balancing = true
 
   tags = {
-    Name = "vcs_lb"
+    Name = "vcs_alb"
   }
-
 }
-  
-
 
